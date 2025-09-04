79c7d54be96a5fc58e82e93acb514e168e17ac9e
Clinton <clintonchakma.24csedu.064@gmail.com>
2025-09-04 21:35:59 +0200

    Room Service

---
+++ Chat-app/Models/Room.cs
@@ -0,0 +1,47 @@
+﻿namespace Chat_app.Models;
+
+public class Room
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+
+	public List<string> Users { get; set; } = new List<string>();
+
+	public List<string> Messages { get; set; } = new List<string>();
+
+	public Room(int id, string name)
+	{
+		Id = id;
+		Name = name;
+	}
+
+	public void AddUser(string userName)
+	{
+		if (!Users.Contains(userName))
+		{
+			Users.Add(userName);
+		}
+	}
+
+	public void RemoveUser(string userName)
+	{
+		if (Users.Contains(userName))
+		{
+			Users.Remove(userName);
+		}
+	}
+
+	public void AddMessage(string message)
+	{
+		Messages.Add(message);
+	}
+
+	public void ClearMessages()
+	{
+		Messages.Clear();
+	}
+	public override string ToString()
+	{
+		return $"Room: {Name}, Users: {string.Join(", ", Users)}, Messages: {string.Join("; ", Messages)}";
+	}
+}
--- Chat-app/Program.cs
+++ Chat-app/Program.cs
@@ -1,19 +1,22 @@
 using Chat_app.Hubs;
+using Chat_app.Services;
+using Chat_app.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
+builder.Services.AddScoped<IRoomService, RoomService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
